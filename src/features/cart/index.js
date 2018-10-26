ef779dd93d2367eca04b4a92c45c5961a655f15a
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-27 08:02:33 +1300

    add and remove

--- src/features/cart/index.js
+++ src/features/cart/index.js
@@ -1,2 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
+function Cart(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Quantity</th>
+          <th />
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {props.cart.map(item => {
+          <tr>
+            <td> {item.name}</td>
+            <td> {item.quantity}</td>
+            <td>
+              <button onClick={e => props.addToCart()}>+</button>
+            </td>
+            <td>
+              <button onClick={e => props.removeFromCart()}>-</button>
+            </td>
+          </tr>;
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cart
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToCart: item => {
+      dispatch({ type: 'ADD', payload: item });
+    },
+    removeFromCart: item => {
+      dispatch({ type: 'REMOVE', payload: item });
+    }
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Cart);
